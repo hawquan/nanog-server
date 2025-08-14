9b9bb87cbf394de4dd8fd673416c89ff036ec19d
ChingHong19980819 <ychong980819@gmail.com>
2025-08-14 18:48:39 +0800

    Credential changes on Prod Env

--- HelperFile/cleanup_refund.js
+++ HelperFile/cleanup_refund.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'nanog',
-  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'postgres', //nanog
+  password: process.env.DB_PASSWORD || 'root', //password
   port: process.env.DB_PORT || 5432,
 });
 
